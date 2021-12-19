62d6c474e06e5174d0057839ad0c7409d99a091c
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2021-12-19 18:56:43 +0200

    add facial route

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
+import ntpath
+import os
+from pathlib import Path
+import cv2
 from flask import Flask, redirect, url_for, request, render_template
+import numpy as np
+from easy_facial_recognition import easy_face_reco, encode_face
+import PIL.Image
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -37,6 +44,43 @@ def login():
    else:
       user = request.args.get('nm')
       return redirect(url_for('success',name = user))
+
+@app.route('/facial')
+def facial():
+    print('[INFO] Importing faces...')
+    face_to_encode_path = Path('./known_faces')
+    files = [file_ for file_ in face_to_encode_path.rglob('*.jpg')]
+
+    for file_ in face_to_encode_path.rglob('*.png'):
+        files.append(file_)
+    if len(files)==0:
+        raise ValueError('No faces detect in the directory: {}'.format(face_to_encode_path))
+    known_face_names = [os.path.splitext(ntpath.basename(file_))[0] for file_ in files]
+
+    known_face_encodings = []
+    for file_ in files:
+        image = PIL.Image.open(file_)
+        image = np.array(image)
+        face_encoded = encode_face(image)[0][0]
+        known_face_encodings.append(face_encoded)
+
+    print('[INFO] Faces well imported')
+    print('[INFO] Starting Webcam...')
+    video_capture = cv2.VideoCapture(0)
+    print('[INFO] Webcam well started')
+    print('[INFO] Detecting...')
+    while True:
+        ret, frame = video_capture.read()
+        easy_face_reco(frame, known_face_encodings, known_face_names)
+        cv2.imshow('Easy Facial Recognition App', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    print('[INFO] Stopping System')
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+    return render_template('facial.html')
+
 
 
 app.add_url_rule('/ici', 'g2g', gfg)
